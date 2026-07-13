f744b6abdfb0a12fca41853d1802b8ae0d068ee2
Vaibhav Verma <vaibhav1812207@gmail.com>
2026-07-13 21:38:39 +0530

    It shows simple intro about me

---
+++ Cbasics/Biodata.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+    int age=18;
+    char *name="Vaibhav Verma";
+    char *College="KIET";
+    printf("Name of student is %s \n",name);
+    printf("Age of student is %d \n",age);
+    printf("College of student is %s \n",College);
+    return 0;
+}

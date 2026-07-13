6d3e09c75de7d327645a7753ffa7cdd33d805f03
Vaibhav Verma <vaibhav1812207@gmail.com>
2026-07-13 19:44:37 +0530

    Learned the structure and added comments to it

--- Cbasics/hello.c
+++ Cbasics/hello.c
@@ -1,6 +1,6 @@
-# include<stdio.h>
+#include<stdio.h> // This is used to start the program
 
-int main() {
-    printf("Helloo yay first program");
-    return 0;
+int main() { //This is main function where the program starts its execution line by line
+    printf("Hellow world!!"); // this is used to print what is asked
+    return 0; // It returns the code has 0 error and is executed
 }
